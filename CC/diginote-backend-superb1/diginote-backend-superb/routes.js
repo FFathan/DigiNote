28d060b682b4780ce41e614c6d55401026486e9c
DavidNbElectricalEngineer <125957149+DavidNbEE@users.noreply.github.com>
2023-06-04 14:38:43 +0700

    Delete CC/diginote-backend-superb1 directory

--- CC/diginote-backend-superb1/diginote-backend-superb/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const { registerHandler, loginHandler } = require('./reg-log-api/handler')
-const router = express.Router()
-
-router.post('/register', registerHandler)
-router.post('/login', loginHandler)
-
-module.exports = router
