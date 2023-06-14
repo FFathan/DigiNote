460106466b255d500cc93450864dde9238c942bc
DavidNbElectricalEngineer <125957149+DavidNbEE@users.noreply.github.com>
2023-06-14 15:57:12 +0700

    Update testjwt.js

--- CC/reg-log-api/testjwt.js
+++ CC/reg-log-api/testjwt.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 const secret = "diginote-secret"
 
-token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIsImlhdCI6MTY4NjUyMzQzNn0.0InSB5rDXhw0-EnBI2rRYxqSZ33X--ky1KCQpUf48mE'
+token = 'your-token'
 const decoded = jwt.decode(token)
 
 const authToken = jwt.verify(token, secret)
